0eadf8230b402697b5e106cbcf072df1faa5296b
xiaoran02 <757240203@qq.com>
2022-02-23 22:47:22 +0800

    add error.jsp Perfect Servlet business

--- src/main/java/com/news/servlet/UserServlet.java
+++ src/main/java/com/news/servlet/UserServlet.java
@@ -1,8 +1,12 @@
 package com.news.servlet;
 
+import com.news.entity.User;
+import com.news.service.impl.UserServiceImpl;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/login")
 public class UserServlet implements Servlet {
@@ -11,10 +15,41 @@ public class UserServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
 
-        //获取表单提交信息
-        Object username =  servletRequest.getAttribute("uname");
-        Object password =  servletRequest.getAttribute("upwd");
+        //新建ServiceImpl对象
+        UserServiceImpl userService = new UserServiceImpl();
 
+        //获取表单提交信息
+        String username =  servletRequest.getParameter("uname");
+        String password =  servletRequest.getParameter("upwd");
+
+        //检验
+//        System.out.println(username);
+//        System.out.println(password);
+
+        //新建一个用户
+        User user = new User();
+        //传递参数
+        user.setUpwd(password);
+        user.setUname(username);
+
+
+
+        try {
+            User u = userService.doLogin(user);
+//            System.out.println(u);
+            servletRequest.setAttribute("User",u);
+            if(u != null){
+
+                servletRequest.getRequestDispatcher("newspages/admin.jsp").forward(servletRequest,servletResponse);
+            }
+            else{
+                servletRequest.getRequestDispatcher("error.jsp").forward(servletRequest,servletResponse);
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
---
+++ src/main/webapp/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: administer
+  Date: 2022/2/23
+  Time: 22:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>错误界面</title>
+</head>
+<body>
+<h1>用户你好，你的密码或用户名大概也许似乎是输错了~</h1>
+</body>
+</html>
